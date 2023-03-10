eb8bb509efbad381e0eb76af006d2d38c4dfc229
“Noah <njeans@ualberta.ca>
2023-03-09 19:30:25 -0700

    added the classes for the taskbar buttons v2

---
+++ app/src/main/java/com/example/barqrxmls/LeaderBoard.java
@@ -0,0 +1,18 @@
+/**
+ * @author Noah Jeans
+ * @version 1
+ * This class connects to the leaderboard_screen.xml file
+ */
+
+package com.example.barqrxmls;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LeaderBoard extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.leaderboard_screen);
+    }
+}

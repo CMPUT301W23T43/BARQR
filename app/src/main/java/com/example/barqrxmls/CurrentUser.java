9056c52d05ac5db925b27e5e5ac354cf931aa5ee
tjpollom <tjpollom@ualberta.ca>
2023-03-25 13:08:52 -0600

    Create a CurrentUser singleton container
    This class is a simple singleton container for the User class. It contains the user that is going to be treated as the App's current user, to simplify the work for classes that may need to access them.

---
+++ app/src/main/java/com/example/barqrxmls/CurrentUser.java
@@ -0,0 +1,44 @@
+package com.example.barqrxmls;
+
+/***
+ * Simple singleton container class for the CurrentUser, so that any class which wants to act upon
+ * or reference the current user can just get this instance and work with it.
+ */
+public class CurrentUser {
+    private static CurrentUser instance = null;
+    private User user;
+
+    /***
+     * Empty constructor used privately
+     */
+    private CurrentUser() {
+    }
+
+    /***
+     * Get the instance of the CurrentUser class
+     * @return instance
+     */
+    public CurrentUser getInstance() {
+        if (instance == null) {
+            instance = new CurrentUser();
+        }
+
+        return instance;
+    }
+
+    /***
+     * Sets the CurrentUser's user field.
+     * @param user The user to become the CurrentUser
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /***
+     * Get the CurrentUser's User object
+     * @return User -- the current user object
+     */
+    public User getUser() {
+        return user;
+    }
+}

1001befee0f47e8b21aa33679a80a4ec23469241
Danielle Hopfe <dhopfe@ualberta.ca>
2023-03-25 13:29:07 -0600

    starting to set up NewCode

--- app/src/main/java/com/example/barqrxmls/NewCode.java
+++ app/src/main/java/com/example/barqrxmls/NewCode.java
@@ -1,18 +1,26 @@
 /**
  * @author Noah Jeans
  * @version 1
- * This class connects to the barqr_code.xml file
+ * This class connects to the
  */
 
 package com.example.barqrxmls;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewCode extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.barqr_code);
+        setContentView(R.layout.code_screen);
+        Bundle bundle = getIntent().getExtras();
+        Code code = (Code)bundle.get("code");
+
+        TextView codeName = findViewById(R.id.code_name);
+        codeName.setText(String.format("Name: %s",code.getName()));
+        
     }
 }

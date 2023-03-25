ad9709d8cfdb92756c1a287817ba4bac23f52cd8
Anjelica Marianicz <anjelica@ualberta.ca>
2023-03-25 14:52:53 -0600

    Added email and username
    Added username and email to be displayed for the current user on the accounts page.

--- app/src/main/java/com/example/barqrxmls/Account.java
+++ app/src/main/java/com/example/barqrxmls/Account.java
@@ -7,13 +7,61 @@
 package com.example.barqrxmls;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import org.checkerframework.checker.units.qual.Current;
+
 public class Account extends AppCompatActivity {
+
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
+    private CollectionReference usersRef = database.collection("Users");
+
+    String usernameValue;
+
+    TextView username;
+    TextView email;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_screen);
+
+        CurrentUser currentUser = CurrentUser.getInstance();
+
+        User thisUser = currentUser.getUser();
+
+        username = findViewById(R.id.usernameMyAccount);
+
+        email = findViewById(R.id.emailMyAccount);
+
+        Button closeButton = (Button) findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        usernameValue = thisUser.getUserName();
+        username.setText(usernameValue);
+        email.setText(thisUser.getEmail());
+
+
     }
 
 }

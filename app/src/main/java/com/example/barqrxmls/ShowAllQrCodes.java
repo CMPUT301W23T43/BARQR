3abff2f34482342b9590dc4c408d13263721e295
KayKay321 <87404888+KayKay321@users.noreply.github.com>
2023-03-26 14:27:54 -0600

    done with displaying all codes

--- app/src/main/java/com/example/barqrxmls/ShowAllQrCodes.java
+++ app/src/main/java/com/example/barqrxmls/ShowAllQrCodes.java
@@ -1,12 +1,19 @@
 package com.example.barqrxmls;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -25,14 +32,28 @@ public class ShowAllQrCodes extends AppCompatActivity {
         setContentView(R.layout.activity_show_all_qr_codes);
         AllCodesList = findViewById(R.id.codesList);
         AllCodeDataList = new ArrayList<>();
-        Code testCode1 = new Code("/usr/code1");
-        Code testCode2 = new Code(";lkajsdf");
-        AllCodeDataList.add(testCode1);
-        AllCodeDataList.add(testCode2);
+        Query getUsersByScores = codesRef.orderBy("points", Query.Direction.DESCENDING);
 
-        CodeAdapter = new CodeArrayAdapter(this, AllCodeDataList);
-        AllCodesList.setAdapter(CodeAdapter);
-        CodeAdapter.notifyDataSetChanged();
+            // Retrieve all documents in the collection
+        getUsersByScores.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Access the document data
+                            Code obj = document.toObject(Code.class);
+                            Log.d("MyActivity", obj.toString());
+                            AllCodeDataList.add(obj);
+                            CodeAdapter = new CodeArrayAdapter(ShowAllQrCodes.this, AllCodeDataList);
+                            AllCodesList.setAdapter(CodeAdapter);
+                            CodeAdapter.notifyDataSetChanged();
+                        }
+                    } else {
+                        // Handle errors here
+                        Log.e("MyActivity", "Error getting documents: " + task.getException().getMessage());
+                    }
+                }
+            });
 
     }
 }

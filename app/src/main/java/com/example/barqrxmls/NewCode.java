70592ee007cc3f4a6a851c98ca1db8163647ffda
Danielle Hopfe <dhopfe@ualberta.ca>
2023-03-29 18:10:19 -0600

    started hooking up NewCode

--- app/src/main/java/com/example/barqrxmls/NewCode.java
+++ app/src/main/java/com/example/barqrxmls/NewCode.java
@@ -6,21 +6,79 @@
 
 package com.example.barqrxmls;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
 
 public class NewCode extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.code_screen);
+
+        // get code and user
         Bundle bundle = getIntent().getExtras();
         Code code = (Code)bundle.get("code");
+        User user = CurrentUser.getInstance().getUser();
 
+        // set Name field
         TextView codeName = findViewById(R.id.code_name);
         codeName.setText(String.format("Name: %s",code.getName()));
-        
+
+        // set comment field
+        if(user.hasComment(code.getHash())) {
+            String comment = user.getCodes().get(code.getHash());
+            TextView codeComment = findViewById(R.id.commentField);
+            codeComment.setText(comment);
+        }
+
+        // set point value
+        TextView pointValue = findViewById(R.id.point_value);
+        pointValue.setText(String.format(Locale.CANADA,"Point Value: %d",code.getPoints()));
+
+        // set geolocation (UNFINISHED AS USER DOES NOT STOre CODE GEOLOCATION YET, CURRENTLY
+        // USING PLACEHOLDER VALUE
+        TextView geolocation = findViewById(R.id.code_geolocation);
+        String STANDIN = "stand-in for geolocation";
+        geolocation.setText(String.format(Locale.CANADA,"Geolocation: %s",STANDIN));
+
+        //TODO: add proper geolocation, unique image, and surroundings image, setup edit feature for comment
+
+        // setup close button
+        Button close = findViewById(R.id.close_button);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        // setup delete button
+        Button delete = findViewById(R.id.delete_code_button);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog = new AlertDialog.Builder(NewCode.this).
+                        setTitle("Delete Code").
+                        setMessage("Are you sure you want to delete this Code?").
+                        setPositiveButton("Yes", (dialog, which) -> {
+                            user.removeCode(code.getHash(), code.getPoints());
+                            finish();
+                        }).
+                        setNegativeButton("No", (dialog, which) -> {
+                        }).
+
+                        create();
+                alertDialog.show();
+            }
+        });
     }
 }

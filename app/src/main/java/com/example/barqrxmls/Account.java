4c9a1cecde695ab0d7b200603ac372e539a8b8e9
Anjelica Marianicz <anjelica@ualberta.ca>
2023-03-31 22:08:30 -0600

    Added code rank
    Added code ranking to account page where highest and lowest scoring code names are displayed.

--- app/src/main/java/com/example/barqrxmls/Account.java
+++ app/src/main/java/com/example/barqrxmls/Account.java
@@ -7,7 +7,6 @@
 package com.example.barqrxmls;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,12 +21,13 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Account extends AppCompatActivity {
@@ -40,14 +40,18 @@ public class Account extends AppCompatActivity {
 
     TextView username;
     TextView email;
+    TextView highCodeRank;
 
     Integer highestScore = 0;
     Integer lowestScore = 0;
+    Integer rank = 0;
     String highCode;
     String lowCode;
 
     TextView highCodeName;
     TextView lowCodeName;
+    DocumentReference userInDatabase;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +67,7 @@ public class Account extends AppCompatActivity {
 
         highCodeName = findViewById(R.id.highestQRCode);
         lowCodeName = findViewById(R.id.lowestQRCode);
+        highCodeRank = findViewById(R.id.highCodeHash);
 
         highCodeName.setVisibility(View.INVISIBLE);
         lowCodeName.setVisibility(View.INVISIBLE);
@@ -79,9 +84,19 @@ public class Account extends AppCompatActivity {
         username.setText(usernameValue);
         email.setText(thisUser.getEmail());
 
-        DocumentReference userInDatabase = usersRef.document(thisUser.getUserName());
+        userInDatabase = usersRef.document(thisUser.getUserName());
 
-        userInDatabase.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        Query codesByScores = codesRef.orderBy("points", Query.Direction.DESCENDING);
+        getCodeRank(codesByScores);
+
+
+
+        highCodeName.setVisibility(View.VISIBLE);
+        lowCodeName.setVisibility(View.VISIBLE);
+    }
+
+    private void getPolarCodes(DocumentReference docRef, LinkedHashMap<String, Integer> codesRank) {
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 HashMap<Object, Object> userCodes = new HashMap<>();
@@ -98,23 +113,48 @@ public class Account extends AppCompatActivity {
                                 highCode = (String) documentSnapshot.get("name");
                                 highestScore = (Long.valueOf((Long) documentSnapshot.get("points")).intValue());
                                 highCodeName.setText(highCode);
-                            }
+                                highCodeRank.setText("number "+codesRank.get((String) documentSnapshot.get("hash"))+" of "+codesRank.size()+"!");
+
+                                }
                             if (Long.valueOf((Long) documentSnapshot.get("points")).intValue() < lowestScore || lowestScore == 0) {
                                 lowCode = (String) documentSnapshot.get("name");
                                 lowestScore = Long.valueOf((Long) documentSnapshot.get("points")).intValue();
                                 lowCodeName.setText(lowCode);
-
                             }
 
                         }
                     });
+                }
+            }
+
+        });
+    }
+
+    private void getCodeRank(Query query){
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                ArrayList<String> codes = new ArrayList<>();
+                LinkedHashMap<String, Integer> codesRank = new LinkedHashMap<>();
+
+                if (task.isSuccessful()) {
+                    int i = 0;
+                    Double topScore = Double.POSITIVE_INFINITY;
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        //System.out.println(highCodeHash);
+                        //System.out.println((String) document.get("hash"));
+                        if (Long.valueOf((Long) document.get("points")).doubleValue() < topScore){
+                            topScore = Long.valueOf((Long) document.get("points")).doubleValue();
+                            i++;
+                        }
+                        codesRank.put((String) document.get("hash"), i);
+                        }
+                    System.out.println(codesRank.entrySet());
+                    getPolarCodes(userInDatabase, codesRank);
+                    }
 
                 }
-
-            }
         });
-
-        highCodeName.setVisibility(View.VISIBLE);
-        lowCodeName.setVisibility(View.VISIBLE);
     }
 }
+
